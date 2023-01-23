b4a144cc319c23cdcaeaa3b951f94b0f226d9af0
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-01-23 16:04:39 +0400

    week2

---
+++ week2/day1/exercises/exercise 2/main.js
@@ -0,0 +1,12 @@
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+let myWatchedSeriesLength = "3";
+
+let myWatchedSeriesSentence = "black mirror, money heist, the big bang theory "
+
+let str = "i watched 3 series"
+
+console.log ("i watched 3 series") + 
+
+
+
