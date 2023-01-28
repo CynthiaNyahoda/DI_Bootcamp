1e4642584417cadd7a77f03959f0b4a83d261da6
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-01-28 08:27:55 +0400

    submit

---
+++ week1/day4/examples/main.js
@@ -0,0 +1,10 @@
+const answer = prompt ("introduce yourself")
+
+
+const words = answer.split(",")
+console.log(answer)
+function log(username){ {
+    console.log(username)}
+    username.forEach(words);}
+
+  
--- week2/day4/exercises/main.js
+++ week2/day4/exercises/main.js
@@ -1,22 +1,83 @@
 // question 1
-const username = prompt("introduce yourself"); //prompt
+let username = prompt("introduce yourself"); //prompt
 console.log(username)
 
 // // question 2
-
-
 function slitify(username){
 return username.split(/\W/)
 
 }
 console.log(slitify(username))
 
+
 // question 3
-let usernames = ['Hello', 'my', 'name', 'is', 'Cynthia']
-for (let i = 0; i < usernames.length; i++) {
-   console.log(usernames[i])
+
+function lengthofLongestWord(){ 
+   
+   let longestwordlength = 0;
+   
+      let usernames =  ['Hello', 'my', 'name', 'is', 'wendy']
+      for (let i = 0; i < usernames.length; i++) {
+      console.log(usernames[i])
+   
+   
+      const wordlength = usernames.length
+      if(wordlength > longestwordlength){
+         longestwordlength = wordlength
+      }
+   
+      }return longestwordlength }
+
+const longestwordlength = lengthofLongestWord()
+const firstRow = createFirstRow();
+console.log(firstRow);
+
+function createFirstRow() {
+   const numberfirstRowStars = longestwordlength + 5;
+   let row = "";
+   for (let i = 0; i< numberfirstRowStars; i++) {
+    row = row + "*";
     
+   }
+   return row
 }
 
-let lengthoflongestword = 0;
-const wordlength
+
+
+function displayRows() {
+   const numberofsideStars = 4
+   const numberfirstRowStars = longestwordlength + 
+numberofsideStars;
+
+return "*".repeat(numberfirstRowStars)
+   let row = "";
+   for (let i = 0; i< numberfirstRowStars; i++) {
+    row = row + "*";
+   
+} }
+
+
+
+const delimiterRow = createDelimterRow();
+   console.log(delimiterRow);
+      for(const word of words) 
+      displayWord(word);
+   
+ 
+
+function displayWord(word) {
+  
+   const numberOfSpacesToAdd = longestwordlength - word.length + 1
+   const emptySpaces = "".repeat(numberOfSpacesToAdd);
+   console.log("*" + word + emptySpaces + "*");
+}
+
+
+
+
+
+
+
+
+
+
