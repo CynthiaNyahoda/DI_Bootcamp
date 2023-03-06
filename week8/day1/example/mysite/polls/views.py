ea309fd407ebae97cb116c06182078ae79dc5889
CynthiaNyahoda <122863867+CynthiaNyahoda@users.noreply.github.com>
2023-03-06 23:26:20 +0400

    Add files via upload

---
+++ week8/day1/example/mysite/polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+from django.http import HttpResponse # pass view information into the browser
+
+# takes a request, returns a response
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
